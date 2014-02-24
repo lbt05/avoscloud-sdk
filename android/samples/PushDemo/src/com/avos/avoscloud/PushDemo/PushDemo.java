7e48d372faacf6db28da01dbb017daef8694c66f
lbt05 <lbt0506@gmail.com>
2014-02-24 17:47:38 +0800

    add pushService package

--- android/samples/PushDemo/src/com/avos/avoscloud/PushDemo/PushDemo.java
+++ android/samples/PushDemo/src/com/avos/avoscloud/PushDemo/PushDemo.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import com.avos.avoscloud.*;
+import com.avos.avospush.*;
 
 public class PushDemo extends Activity {
     /**
